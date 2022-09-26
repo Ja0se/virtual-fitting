deea116154670c268522270b0a0e1b0357e2b0d8
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-09-26 17:20:38 +0900

    Update musinsa2.py

--- musinsa2.py
+++ musinsa2.py
@@ -1,5 +1,10 @@
 import re
 import time
+import base64
+import requests
+import numpy as np
+from PIL import Image
+from io import BytesIO
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -11,8 +16,8 @@ options = Options()
 # options.add_argument()
 
 url = 'https://www.musinsa.com/mz/brandsnap'
-s = Service('./chromedriver')
-driver = webdriver.Chrome(service = s, options=options)
+# s = Service('./chromedriver.exe')
+driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 driver.get(url)
 
 totalPage = driver.find_element(By.CLASS_NAME, 'totalPagingNum').text
@@ -29,9 +34,13 @@ for item in range(totalPage//10-1):
         imgList = driver.find_elements(By.CSS_SELECTOR, '.articleImg > a > img')
         
         for img in imgList:
-            totalImg.append(img.get_attribute('src'))
-            print(totalImg)
-       
+            #totalImg.append(base64.b64encode(requests.get(img.get_attribute('src')).content))
+            # totalImg.append(img.get_attribute('src'))
+            res = requests.get(img.get_attribute('src'))
+            img = Image.open(BytesIO(res.content))
+            npimg = np.array(img)
+            print(npimg)
+            
         pageBox = driver.find_elements(By.CSS_SELECTOR, '.pagination > div > a')
     pageBox[12].send_keys('\n')
     time.sleep(1)
