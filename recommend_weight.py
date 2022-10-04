d3ef4cddeda9dcec0cf64f046ecce48705de283f
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-10-04 22:53:07 +0900

    Update recommend_weight.py

--- recommend_weight.py
+++ recommend_weight.py
@@ -1,22 +1,15 @@
 import pandas as pd
 
+def Add(x):
+    df=pd.read_csv('color.csv')
+    x=pd.DataFrame(x)
+    x.to_csv('./test.csv',index=False)
+    x=pd.read_csv('test.csv')
+    df= df+ x
+    df.to_csv('./color.csv',index=False)
+    return df
 
-array = [[0]*17 for i in range(17)]
-
-a=pd.DataFrame(array)
-
-
-df=pd.read_csv('color.csv')
-
-print(df)
-print('------------------------------------------------------')
-a[0][2]=3
-a.to_csv('./test.csv',index=False)
-a=pd.read_csv('test.csv')
-
-print('------------------------------------------------------')
-print(a)
-df= df+ a
-print('------------------------------------------------------')
-
-print(df)
+if __name__=='__main__':
+    x=[[0]*17 for i in range(17)]
+    x[0][3]=3
+    Add(x)
