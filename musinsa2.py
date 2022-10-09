ee992c98eeeb0b8b32d229c6277281a2e5089b45
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-10-09 20:27:05 +0900

    Update musinsa2.py

--- musinsa2.py
+++ musinsa2.py
@@ -25,8 +25,9 @@ options = Options()
 # options.add_argument()
 net = infer.U2net()
 Ext=Extract_Color.Extract()
+Ext.Clt(2)
 
-url = 'https://www.musinsa.com/mz/brandsnap'
+url = 'https://www.musinsa.com/mz/streetsnap'
 # s = Service('./chromedriver.exe')
 driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 driver.get(url)
@@ -54,66 +55,70 @@ for item in range(totalPage//10-1):
             '''
             test 시작
             '''
-            u2net_mask=net.Output(img)
-            u2net_mask=u2net_mask.convert('RGB')
-            Up,Bottom,Dress=-1,-1,-1
-            
-            # plt.figure()
-            # plt.imshow(u2net_mask)
-            # plt.show()
-            
-            Div=Divide.Div(u2net_mask,img)
-            #상의 추출
-            x,y,w,h,Div_image=Div.Red()
-            #print(Div_image)
-            if(Div_image is not 0):
-                Div_image=Div_image[x:w,y:h]
-                Ext.fit(Div_image)
-                ExtractImg,perc,k_cluster=Ext.palette_perc()
-                mask_color=0
-                if perc[0]>perc[1]:
+            try :
+                
+                u2net_mask=net.Output(img)
+                u2net_mask=u2net_mask.convert('RGB')
+                Up,Bottom,Dress=-1,-1,-1
+                
+                # plt.figure()
+                # plt.imshow(u2net_mask)
+                # plt.show()
+                
+                Div=Divide.Div(u2net_mask,img)
+                #상의 추출
+                x,y,w,h,Div_image=Div.Red()
+                #print(Div_image)
+                if(Div_image is not 0):
+                    Div_image=Div_image[x:w,y:h]
+                    Ext.fit(Div_image)
+                    ExtractImg,perc,k_cluster=Ext.palette_perc()
                     mask_color=0
-                else:
-                    mask_color=1
-                #print(perc[mask_color])
-                #print(k_cluster.cluster_centers_[mask_color])
-                Up=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
-            
-            #하의 추출
-            x,y,w,h,Div_image=Div.Green()
-            #print(Div_image)
-            if(Div_image is not 0):
-                Div_image=Div_image[x:w,y:h]
-                Ext.fit(Div_image)
-                ExtractImg,perc,k_cluster=Ext.palette_perc()
-                mask_color=0
-                if perc[0]>perc[1]:
+                    if perc[0]>perc[1]:
+                        mask_color=0
+                    else:
+                        mask_color=1
+                    #print(perc[mask_color])
+                    #print(k_cluster.cluster_centers_[mask_color])
+                    Up=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+                
+                #하의 추출
+                x,y,w,h,Div_image=Div.Green()
+                #print(Div_image)
+                if(Div_image is not 0):
+                    Div_image=Div_image[x:w,y:h]
+                    Ext.fit(Div_image)
+                    ExtractImg,perc,k_cluster=Ext.palette_perc()
                     mask_color=0
-                else:
-                    mask_color=1
-                #print(perc[mask_color])
-                #print(k_cluster.cluster_centers_[mask_color])
-                Bottom=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
-            
-            #Dress 추출
-            x,y,w,h,Div_image=Div.Yellow()
-            #print(Div_image)
-            if(Div_image is not 0):
-                Div_image=Div_image[x:w,y:h]
-                Ext.fit(Div_image)
-                ExtractImg,perc,k_cluster=Ext.palette_perc()
-                mask_color=0
-                if perc[0]>perc[1]:
+                    if perc[0]>perc[1]:
+                        mask_color=0
+                    else:
+                        mask_color=1
+                    #print(perc[mask_color])
+                    #print(k_cluster.cluster_centers_[mask_color])
+                    Bottom=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+                
+                #Dress 추출
+                x,y,w,h,Div_image=Div.Yellow()
+                #print(Div_image)
+                if(Div_image is not 0):
+                    Div_image=Div_image[x:w,y:h]
+                    Ext.fit(Div_image)
+                    ExtractImg,perc,k_cluster=Ext.palette_perc()
                     mask_color=0
-                else:
-                    mask_color=1
-                #print(perc[mask_color])
-                #print(k_cluster.cluster_centers_[mask_color])
-                Dress=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
-            
-            rw.Add(Up,Bottom,Dress)
-            cnt+=1
-            print('now count : ',count)
+                    if perc[0]>perc[1]:
+                        mask_color=0
+                    else:
+                        mask_color=1
+                    #print(perc[mask_color])
+                    #print(k_cluster.cluster_centers_[mask_color])
+                    Dress=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+                
+                rw.Add(Up,Bottom,Dress)
+                cnt+=1
+                print('now count : ',cnt)
+            except:
+                print('Error', cnt)
             '''
             test 끝
             '''
