11e75b89fd9587861b3b18243c79c985045d39bc
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-10-09 18:00:05 +0900

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,23 +7,24 @@ from PIL import Image
 import Divide
 import copy
 import cv2
-
+import Color
 def imgshow(img):
     plt.figure()
     plt.imshow(img)
     plt.show()
     
 if __name__== '__main__':
+    Up,Bottom,Outer=-1,-1,-1
     net = infer.U2net()
     Ext=Extract_Color.Extract()
-    img_path='./input_images/11.jpg'
+    img_path='./input_images/1234.jpg'
     img=Image.open(img_path).convert('RGB')
     '''
     Extract Color using unet c_model
     '''
     unet=Make_Image.MI(img_path)
     unet_mask=unet.Output()
-    Ext.Clt(4)
+    Ext.Clt(2)
     Ext.fit(unet_mask)
     ExtractImg,perc,k_cluster=Ext.palette_perc()
     #imgshow(ExtractImg)
@@ -33,20 +34,57 @@ if __name__== '__main__':
     '''
     u2net_mask=net.Output(img)
     u2net_mask=u2net_mask.convert('RGB')
-    #print(u2net_mask)
-    # 이거 그거 이미지 디코딩 작업해야할듯
-    #imgshow(u2net_mask)
-    
     
     Div=Divide.Div(u2net_mask,img)
+    #상의 추출
     x,y,w,h,Div_image=Div.Red()
     print(x,y,w,h)
     #print(Div_image)
     if(Div_image is not 0):
         Div_image=Div_image[x:w,y:h]
-        imgshow(Div_image)
+        Ext.fit(Div_image)
+        ExtractImg,perc,k_cluster=Ext.palette_perc()
+        mask_color=0
+        if perc[0]>perc[1]:
+            mask_color=0
+        else:
+            mask_color=1
+        #print(perc[mask_color])
+        #print(k_cluster.cluster_centers_[mask_color])
+        Up=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+    
+    #하의 추출
+    x,y,w,h,Div_image=Div.Green()
+    print(x,y,w,h)
+    #print(Div_image)
+    if(Div_image is not 0):
+        Div_image=Div_image[x:w,y:h]
+        Ext.fit(Div_image)
+        ExtractImg,perc,k_cluster=Ext.palette_perc()
+        mask_color=0
+        if perc[0]>perc[1]:
+            mask_color=0
+        else:
+            mask_color=1
+        #print(perc[mask_color])
+        #print(k_cluster.cluster_centers_[mask_color])
+        Bottom=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+    
+    #외투 추출
+    x,y,w,h,Div_image=Div.Yellow()
+    print(x,y,w,h)
+    #print(Div_image)
+    if(Div_image is not 0):
+        Div_image=Div_image[x:w,y:h]
+        Ext.fit(Div_image)
+        ExtractImg,perc,k_cluster=Ext.palette_perc()
+        mask_color=0
+        if perc[0]>perc[1]:
+            mask_color=0
+        else:
+            mask_color=1
+        #print(perc[mask_color])
+        #print(k_cluster.cluster_centers_[mask_color])
+        Outer=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
         
-    Ext.fit(Div_image)
-    ExtractImg,perc,k_cluster=Ext.palette_perc()
-    imgshow(ExtractImg)
-    print(perc,k_cluster)
+    print(Up,Bottom,Outer)
