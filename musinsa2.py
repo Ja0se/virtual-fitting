04b0a527069678e70a76611cb1f3fcc6b9a2aead
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-10-09 18:21:31 +0900

    Update musinsa2.py

--- musinsa2.py
+++ musinsa2.py
@@ -10,10 +10,21 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
+import Make_Image
+import infer
+import Extract_Color
+import matplotlib.pyplot as plt
+import Divide
+import copy
+import cv2
+import Color
+import recommend_weight as rw
 check = False
 
 options = Options()
 # options.add_argument()
+net = infer.U2net()
+Ext=Extract_Color.Extract()
 
 url = 'https://www.musinsa.com/mz/brandsnap'
 # s = Service('./chromedriver.exe')
@@ -39,8 +50,72 @@ for item in range(totalPage//10-1):
             res = requests.get(img.get_attribute('src'))
             img = Image.open(BytesIO(res.content))
             npimg = np.array(img)
-            print(npimg)
+            #print(npimg)
+            '''
+            test 시작
+            '''
+            u2net_mask=net.Output(img)
+            u2net_mask=u2net_mask.convert('RGB')
+            Up,Bottom,Outer=-1,-1,-1
             
+            plt.figure()
+            plt.imshow(u2net_mask)
+            plt.show()
+            
+            Div=Divide.Div(u2net_mask,img)
+            #상의 추출
+            x,y,w,h,Div_image=Div.Red()
+            #print(Div_image)
+            if(Div_image is not 0):
+                Div_image=Div_image[x:w,y:h]
+                Ext.fit(Div_image)
+                ExtractImg,perc,k_cluster=Ext.palette_perc()
+                mask_color=0
+                if perc[0]>perc[1]:
+                    mask_color=0
+                else:
+                    mask_color=1
+                #print(perc[mask_color])
+                #print(k_cluster.cluster_centers_[mask_color])
+                Up=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+            
+            #하의 추출
+            x,y,w,h,Div_image=Div.Green()
+            #print(Div_image)
+            if(Div_image is not 0):
+                Div_image=Div_image[x:w,y:h]
+                Ext.fit(Div_image)
+                ExtractImg,perc,k_cluster=Ext.palette_perc()
+                mask_color=0
+                if perc[0]>perc[1]:
+                    mask_color=0
+                else:
+                    mask_color=1
+                #print(perc[mask_color])
+                #print(k_cluster.cluster_centers_[mask_color])
+                Bottom=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+            
+            #외투 추출
+            x,y,w,h,Div_image=Div.Yellow()
+            #print(Div_image)
+            if(Div_image is not 0):
+                Div_image=Div_image[x:w,y:h]
+                Ext.fit(Div_image)
+                ExtractImg,perc,k_cluster=Ext.palette_perc()
+                mask_color=0
+                if perc[0]>perc[1]:
+                    mask_color=0
+                else:
+                    mask_color=1
+                #print(perc[mask_color])
+                #print(k_cluster.cluster_centers_[mask_color])
+                Outer=Color.Colorfull(k_cluster.cluster_centers_[mask_color])
+            
+            rw.Add(Up,Bottom,Outer)
+            print(Up,Bottom,Outer)
+            '''
+            test 끝
+            '''
         pageBox = driver.find_elements(By.CSS_SELECTOR, '.pagination > div > a')
     pageBox[12].send_keys('\n')
     time.sleep(1)
