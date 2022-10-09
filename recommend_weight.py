8f4c6bf9afbb9fed6ad1d828f7a3c24914c8204a
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-10-09 18:47:51 +0900

    Update recommend_weight.py

--- recommend_weight.py
+++ recommend_weight.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def Add(Up,Bottom):
+def Add(Up,Bottom,Dress):
     up_bottom=[[0]*17 for i in range(17)]
     
     if Up != -1 and Bottom !=-1:
@@ -12,11 +12,21 @@ def Add(Up,Bottom):
         df1= df1+ up_bottom
         df1.to_csv('./csv/up_bottom.csv',index=False)
     
+    if Dress != -1:
+        x=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        x[Dress]=1
+        x=pd.DataFrame(x)
+        x.to_csv('./csv/test.csv',index=False)
+        x=pd.read_csv('./csv/test.csv')
+        df=pd.read_csv('./csv/Dress.csv')
+        df=df+x
+        df.to_csv('./csv/Dress.csv',index=False)
+        
     
 
 if __name__=='__main__':
     x=[[0]*17 for i in range(17)]
-    Add(x)
+    Add(-1,-1,0)
     # x=pd.DataFrame(x)
     # x.to_csv('./csv/up_bottom.csv',index=False)
     # x.to_csv('./csv/up_outer.csv',index=False)
