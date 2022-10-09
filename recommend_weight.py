71d287115e7a35580596e26191be7f3d07114260
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-10-09 18:39:44 +0900

    Update recommend_weight.py

--- recommend_weight.py
+++ recommend_weight.py
@@ -1,15 +1,23 @@
 import pandas as pd
 
-def Add(x):
-    df=pd.read_csv('color.csv')
-    x=pd.DataFrame(x)
-    x.to_csv('./test.csv',index=False)
-    x=pd.read_csv('test.csv')
-    df= df+ x
-    df.to_csv('./color.csv',index=False)
-    return df
+def Add(Up,Bottom):
+    up_bottom=[[0]*17 for i in range(17)]
+    
+    if Up != -1 and Bottom !=-1:
+        up_bottom[Up][Bottom]=1
+        df1=pd.read_csv('./csv/up_bottom.csv')
+        up_bottom=pd.DataFrame(up_bottom)
+        up_bottom.to_csv('./csv/test.csv',index=False)
+        up_bottom=pd.read_csv('./csv/test.csv')
+        df1= df1+ up_bottom
+        df1.to_csv('./csv/up_bottom.csv',index=False)
+    
+    
 
 if __name__=='__main__':
     x=[[0]*17 for i in range(17)]
-    x[0][3]=3
     Add(x)
+    # x=pd.DataFrame(x)
+    # x.to_csv('./csv/up_bottom.csv',index=False)
+    # x.to_csv('./csv/up_outer.csv',index=False)
+    # x.to_csv('./csv/bottom_outer.csv',index=False)
