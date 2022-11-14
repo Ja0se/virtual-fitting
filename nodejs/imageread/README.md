3839d64e0fcab140cf490e2c187f953acba4a8f2
Ja0se <86642224+Ja0se@users.noreply.github.com>
2022-11-14 21:37:43 +0900

    Add files via upload

--- nodejs/imageread/README.md
+++ nodejs/imageread/README.md
@@ -1,1 +1,7 @@
-!do not read
+### 웹 기본 세팅
+
+nodejs 설치 후
+vscode로 폴더를 열고
+npm install
+
+npm start 하시면 서버 열림니다
---
+++ nodejs/imageread/test.py
@@ -0,0 +1,32 @@
+import sys
+from PIL import Image
+import cv2
+import base64
+import numpy as np
+from io import BytesIO
+import matplotlib.pyplot as plt
+
+def testFunc():
+    f = open("./test.txt","r")
+    read_data=f.read()
+    f.close()
+    #decode
+    img = Image.open(BytesIO(base64.b64decode(read_data)))
+    img= cv2.cvtColor(np.array(img), cv2.COLOR_BGR2GRAY)
+    #encode
+    jpg_img = cv2.imencode('.jpg', img)
+    b64_string = base64.b64encode(jpg_img[1]).decode('utf-8')
+    d= open("./test.txt","w")
+    d.write(str(b64_string))
+    d.close()
+    return 'success'
+    # return 'hi'
+    #data=data.form['data']
+    # imgdata = base64.b64decode(data)
+    # dataBytesIO = io.BytesIO(imgdata)
+    # image = Image.open(dataBytesIO)
+    # return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+    
+if __name__ == '__main__':
+    #print(testFunc(sys.argv[1]))
+    print(testFunc())
